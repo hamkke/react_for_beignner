b16edf21022352aebb276cec6ce96c3320061ac4
hamkke <walwalzz@naver.com>
2023-03-24 18:35:24 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import { useState, useEffect } from "react";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movie/:id" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<Detail />} />
+    </Routes>
   );
 }
 
